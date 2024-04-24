4f23e2d2ae99f3ed510e2c7207ad492d5975089c
Janne Rautakorpi <112503143+JanneRautakorpi@users.noreply.github.com>
2024-04-24 15:01:23 +0300

    Readability optimizations

--- code/main.py
+++ code/main.py
@@ -1,70 +1,58 @@
 import utime
-
-import machine
 from machine import I2C, Pin
-from lcd_api import LcdApi
 from pico_i2c_lcd import I2cLcd
-
-import sys
 from rotary_irq_rp2 import RotaryIRQ
 
-import time
-
-I2C_ADDR     = 0x27
+# Constants
+I2C_ADDR = 0x27
 I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
-
-# i2c lcd
 PIN_SDA = 26
 PIN_SCL = 27
+SLEEP_TIME = 50  # in ms
 
-# rotary encoder
-PIN_CLK1 = Pin(15)
-PIN_DT1 = Pin(14)
-PIN_CLK2 = Pin(16)
-PIN_DT2 = Pin(17)
 
-PIN_BUTTON = Pin(7, Pin.IN, Pin.PULL_UP)
+def init_lcd(sda_pin, scl_pin):
+    '''
+    Initialization for LCD display.
+    '''
+    i2c = I2C(1, sda=Pin(sda_pin), scl=Pin(scl_pin), freq=400000)
+    lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+    lcd.putstr("Welcome to Measurement Fox <3")
+    return lcd
 
-SLEEP_TIME = 50 # in ms
+def init_rotary(pin_clk, pin_dt):
+    '''
+    Initialization for rotary encoder.
+    '''
+    return RotaryIRQ(pin_num_clk=Pin(pin_clk),
+                     pin_num_dt=Pin(pin_dt),
+                     pull_up=True,
+                     range_mode=RotaryIRQ.RANGE_UNBOUNDED)
 
 def main():
-    print("Running main")
-    i2c = I2C(1, sda=machine.Pin(PIN_SDA), scl=machine.Pin(PIN_SCL), freq=400000)
-    lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)    
-    lcd.putstr("welcome to measurement fox <3")
-    
-    r = RotaryIRQ(pin_num_clk=PIN_CLK1,
-              pin_num_dt=PIN_DT1,
-              pull_up=True,
-              range_mode=RotaryIRQ.RANGE_UNBOUNDED)
-    
-    r2 = RotaryIRQ(pin_num_clk=PIN_CLK2,
-              pin_num_dt=PIN_DT2,
-              pull_up=True,
-              range_mode=RotaryIRQ.RANGE_UNBOUNDED)
-    
+    lcd = init_lcd(PIN_SDA, PIN_SCL)
+    r1 = init_rotary(15, 14)
+    r2 = init_rotary(16, 17)
+    button = Pin(7, Pin.IN, Pin.PULL_UP)
     distance_constant = 1
+    val_old1, val_old2 = r1.value(), r2.value()
 
-    val_old = r.value()
-    val_old2 = r2.value()
     while True:
-        val_new = r.value()
-        val_new2 = r2.value()
+        val_new1, val_new2 = r1.value(), r2.value()
 
-        if (val_old != val_new) or (val_old2 != val_new2):
-            val_old = val_new
-            val_old2 = val_new2
-            result = (val_new + val_new2) / 2
-            print('values =', val_new, "ja", val_new2)
-            print('result =', result)
+        if val_old1 != val_new1 or val_old2 != val_new2:
+            val_old1, val_old2 = val_new1, val_new2
+            result = (val_new1 + val_new2) / 2
+            print(f'Values = {val_new1}, {val_new2}')
+            print(f'Result = {result}')
             distance = result / distance_constant
             lcd.clear()
-            lcd.putstr(f"distance pushed:  {distance:.2f} meters")
+            lcd.putstr(f"Distance pushed: {distance:.2f} meters")
 
-        if PIN_BUTTON.value() != 1:
-            print('button pressed')
-            r.reset()
+        if not button.value():
+            print('Button pressed')
+            r1.reset()
             r2.reset()
 
         utime.sleep_ms(SLEEP_TIME)
